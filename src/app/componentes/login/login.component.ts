cccb66034b5e34ef253cd2e48b4dc9d0a95f5637
Andres Wuthrich <wuthrich1080@clearpath.com>
2020-05-28 21:40:24 -0300

    Simon terminado

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -18,10 +18,9 @@ export class LoginComponent implements OnInit {
   progresoMensaje="esperando..."; 
   logeando=true;
   ProgresoDeAncho:string;
-  servicio:LocalStorageService;
-  existe:boolean;
-
+  servicio: LocalStorageService;
   clase="progress-bar progress-bar-info progress-bar-striped ";
+  existe:boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -36,65 +35,59 @@ export class LoginComponent implements OnInit {
   }
 
   Entrar() {
-    if (this.usuario === 'admin' && this.clave === 'admin') 
-    {
+    if (this.usuario === 'admin' && this.clave === 'admin') {
       this.router.navigate(['/Principal']);
     }
-    else{
-
-      this.router.navigate(["/Juegos"]);
-    }
   }
-
   MoverBarraDeProgreso(user:string, clave:string) {
-  
     if(this.servicio.iniciarJugador(user, clave))
     {
-      this.existe=true;  
-
-      this.logeando=false;
-      this.clase="progress-bar progress-bar-danger progress-bar-striped active";
-      this.progresoMensaje="NSA spy..."; 
-      let timer = TimerObservable.create(200, 50);
-      this.subscription = timer.subscribe(t => {
-        console.log("inicio");
-        this.progreso=this.progreso+1;
-        this.ProgresoDeAncho=this.progreso+20+"%";
-        switch (this.progreso) {
-          case 15:
-          this.clase="progress-bar progress-bar-warning progress-bar-striped active";
-          this.progresoMensaje="Verificando ADN..."; 
-            break;
-          case 30:
-            this.clase="progress-bar progress-bar-Info progress-bar-striped active";
-            this.progresoMensaje="Adjustando encriptación.."; 
-            break;
-            case 60:
-            this.clase="progress-bar progress-bar-success progress-bar-striped active";
-            this.progresoMensaje="Recompilando Info del dispositivo..";
-            break;
-            case 75:
-            this.clase="progress-bar progress-bar-success progress-bar-striped active";
-            this.progresoMensaje="Recompilando claves facebook, gmail, chats..";
-            break;
-            case 85:
-            this.clase="progress-bar progress-bar-success progress-bar-striped active";
-            this.progresoMensaje="Instalando KeyLogger..";
-            break;
-            
-          case 100:
-            console.log("final");
-            this.subscription.unsubscribe();
-            this.Entrar();
-//            this.router.navigate(["/Juegos"]);          
-            break;
-        }     
-        this.servicio.iniciarJugador(user, clave);      
-      });
-    }
-    else{
-      this.existe=false;
-    }
+      this.existe=true;
+    
+    this.logeando=false;
+    this.clase="progress-bar progress-bar-danger progress-bar-striped active";
+    this.progresoMensaje="NSA spy..."; 
+    let timer = TimerObservable.create(200, 50);
+    this.subscription = timer.subscribe(t => {
+      console.log("inicio");
+      this.progreso=this.progreso+1;
+      this.ProgresoDeAncho=this.progreso+20+"%";
+      switch (this.progreso) {
+        case 15:
+        this.clase="progress-bar progress-bar-warning progress-bar-striped active";
+        this.progresoMensaje="Verificando ADN..."; 
+          break;
+        case 30:
+          this.clase="progress-bar progress-bar-Info progress-bar-striped active";
+          this.progresoMensaje="Adjustando encriptación.."; 
+          break;
+          case 60:
+          this.clase="progress-bar progress-bar-success progress-bar-striped active";
+          this.progresoMensaje="Recompilando Info del dispositivo..";
+          break;
+          case 75:
+          this.clase="progress-bar progress-bar-success progress-bar-striped active";
+          this.progresoMensaje="Recompilando claves facebook, gmail, chats..";
+          break;
+          case 85:
+          this.clase="progress-bar progress-bar-success progress-bar-striped active";
+          this.progresoMensaje="Instalando KeyLogger..";
+          break;
+          
+        case 100:
+          console.log("final");
+          this.subscription.unsubscribe();
+          this.Entrar();
+          this.router.navigate(["/Juegos"]);
+          break;
+      }     
+      
+      this.servicio.iniciarJugador(user, clave);
+    });
+  }else{
+this.existe=false;
+  }
     //this.logeando=true;
   }
+
 }
